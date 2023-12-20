e0062952cca3c8b8e78f265b5ce01d16d4ccbeb3
Antonin Slv <antonin.sylvestre@orange.fr>
2023-12-20 18:48:30 +0100

    création de compte possiible.

--- modele/user.php
+++ modele/user.php
@@ -4,11 +4,7 @@ class user extends Modele {
     private $connected = false;
     private $ID;
     private $pseudo;
-    private $prenom;
-    private $nom;
-    private $mail;
-    private $adresse;
-    private $numero_tel;
+    private $adrID;
     private $registered;
     private $admin;
 
@@ -17,75 +13,36 @@ class user extends Modele {
     public function __construct() {
     }
 
-    public function createUser() {
+
+    /**
+     * Fonction qui créé un utilisateur de A à Z à partir des données POST
+     * @return mixed : une chaine donnant l'erreur, true si tout s'est bien passé
+     */
+    public function registerUserPOST() {
         //vérification du pseudo
         $sql="SELECT * FROM logins WHERE username =\"". $_POST['pseudo'] ."\"";
-        $resultat = $this->executerRequete($sql);
-        if ($resultat) {
+        if ($this->executerRequete($sql)) {//si il y a un résultat, alors le pseudo est déjà pris
             return "Pseudo";
         }
-        /*
-        //init des variables
-        $this->prenom = $_POST['prenom'];
-        $this->nom = $_POST['nom'];
-        $this->mail = $_POST['mail'];
-        $this->numero_tel = $_POST['numero_tel'];
-        $this->registered = true;
-        $this->admin = false;
-        $this->pseudo = $_POST['pseudo'];
+        //vérification de l'adresse
+        if (!(isset($_POST['numero_rue']) && isset($_POST['rue']) && isset($_POST['ville']) && isset($_POST['code_postal']) && isset($_POST['info_supp']))) {
+            return "Adresse";
+        }
+        //vérfication info user 
+        if (!(isset($_POST['mail']) && isset($_POST['mdp']) && isset($_POST['nom']) && isset($_POST['prenom']) )) {
+            return "InfoUser";
+        }
 
-        //on récupère les infos de l'adresse
-        $this->adresse['numero_rue'] = $_POST['numero_rue'];
-        $this->adresse['rue'] = $_POST['rue'];
-        $this->adresse['ville'] = $_POST['ville'];
-        $this->adresse['code_postal'] = $_POST['code_postal'];
+        $this->createAdresseFromPOST();//création adresse
+        $this->adrID = $this->getBDD()->lastInsertId();
 
-        //on insère l'adresse dans la bdd
-        $sql="INSERT INTO adresse (numero_rue, rue, ville, code_postal) ";
-        $sql.="VALUES (:num, :rue, :ville, :codePostal)";
-        $this->executerRequete($sql,
-            array(
-                'num' => $this->adresse['numero_rue'],
-                'rue' => $this->adresse['rue'],
-                'ville' => $this->adresse['ville'],
-                'codePostal' => $this->adresse['code_postal']
-            )
-        );
+        $this->createUserFromPOST($this->adrID);//on insère le user dans la bdd
 
-        //on récupère l'id de l'adresse avec la dernière insertion
-        $this->adresse['id'] = $this->getBDD()->lastInsertId();
+        $this->ID = $this->getBDD()->lastInsertId(); //on récupère l'id du user avec la dernière insertion
+        $this->pseudo = $_POST['pseudo'];//on récupère le pseudo
 
-        //on insère le user dans la bdd
-        $sql="INSERT INTO customer (first_name, last_name, email, phone_number, registered, admin, address_id) ";
-        $sql.="VALUES (:prenom, :nom, :mail, :numeroTel, :registered, :admin, :adresseId)";
-        $this->executerRequete($sql,
-            array(
-                'prenom' => $this->prenom,
-                'nom' => $this->nom,
-                'mail' => $this->mail,
-                'numeroTel' => $this->numero_tel,
-                'registered' => $this->registered,
-                'admin' => $this->admin,
-                'adresseId' => $this->adresse['id']
-            )
-        );
-
-        //on récupère l'id du user avec la dernière insertion
-        $this->ID = $this->getBDD()->lastInsertId();
-
-        //on hash le mdp
-        $this->mdp = password_hash($_POST['mdp'], PASSWORD_DEFAULT);
-
-        //on insère le mdp hashé dans la bdd
-        $sql="INSERT INTO logins (Customer_id, password, username) ";
-        $sql.="VALUES (:id, :mdp, :pseudo)";
-        $this->executerRequete($sql,
-            array(
-                'id' => $this->ID,
-                'mdp' => $this->mdp,
-                'pseudo' => $this->pseudo
-            )
-        );
-        */
+        $this->createLogin($this->ID,$this->pseudo);//on créé son login 
+        
+        return true;
     }
 }
